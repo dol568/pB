69cd7df2df4a3bbcb4357135b4409f2d062be327
Dariusz Olszewski <dariusz.olszewski@mailfence.com>
2023-11-22 11:48:43 +0100

    update starwars.js

--- js03/starwars.js
+++ js03/starwars.js
@@ -1,23 +1,15 @@
 const films = require("./sw-films.json");
 const planets = require("./sw-planets.json");
-const people2 = require("./sw-people.json");
+const people = require("./sw-people.json");
 const starships = require("./sw-starships.json");
 
 // count sum of all starships cost from episodes 4-6
-// console.log(
-//     "Sum of all starships cost from episodes 4 - 6 is: " +
-//     sumAllStarshipsCostFromEpisodes(4, 6)
-// );
 
-// console.log(sumAllStarshipsCostFromEpisodes(4, 6))
+console.log('\n' +
+    "Sum of all starships cost from episodes 4 - 6 is: " +
+    sumAllStarshipsCostFromEpisodes(4, 6) + '\n'
+);
 
-function sumAllStarshipsCostFromEpisodes2(startEp, endEp) {
-    let sum = 0;
-    return starships
-        .map((starship) => {
-            return starship.cost_in_credits
-        })
-}
 
 function sumAllStarshipsCostFromEpisodes(startEp, endEp) {
     let sum = 0;
@@ -37,11 +29,10 @@ function sumAllStarshipsCostFromEpisodes(startEp, endEp) {
 
 // find the fastest starship you can afford having 8500000 credits
 
-// console.log(
-//     "Fastest ship I can get for up to 8500000 is: " +
-//     getFastestShipFor(8500000)
-// );
-
+console.log(
+    "Fastest ship I can get for up to 8500000 is: " +
+    getFastestShipFor(8500000) + '\n'
+);
 
 function getFastestShipFor(money) {
     return starships
@@ -61,7 +52,7 @@ function getFastestShipFor(money) {
 
 console.log(
     "Planet name with the lowest difference between the rotation period and orbital period is: " +
-    getPlanetNameWithLowestDifference("rotation_period", "orbital_period")
+    getPlanetNameWithLowestDifference("rotation_period", "orbital_period") + '\n'
 );
 
 function getPlanetNameWithLowestDifference(key1, key2) {
@@ -75,14 +66,19 @@ function getPlanetNameWithLowestDifference(key1, key2) {
             let diff = Math.abs(planet[key1] - planet[key2]);
             return {diff, name: planet.name}
         })
-        .sort((a, b) => a.diff - b.diff)[0].name;
+        .sort((a, b) => a.diff - b.diff)
+        .filter((planet, index, array) => {
+            return planet.name === array[0].name;
+        })
+        .map(planet => planet.name)
 }
 
 // map all starships with crew <= 4 that were created between 10 dec 2014 and 15 dec 2014
 
 console.log(
-    "Ships with max crew of 4 created between 10.12.2014 - 12.12.2014 number is: " +
-    getCrewShipFrom(4, new Date(2014, 11, 10), new Date(2014, 11, 12)).length
+    "Ships with max crew of 4 created between 10.12.2014 - 15.12.2014 number is: " +
+    getCrewShipFrom(4, new Date(2014, 11, 10), new Date(2014, 11, 15)).length
+    + '\n'
 );
 
 function getCrewShipFrom(maxCrew, dateStart, dateEnd) {
@@ -105,44 +101,31 @@ function getCrewShipFrom(maxCrew, dateStart, dateEnd) {
 // create an array of people’s names from episodes 1 and 5 sorted by the diameter of origin planet low to high
 
 console.log(
-//     // "People from ep 1 - 5 sorted by origin planet diameter low to high: " +
+    "People from ep 1 - 5 sorted by origin planet diameter low to high: " +
     getPeopleSortedByOriginPlanetDiameter(1, 5)
+    + '\n'
 );
 
 function getPeopleSortedByOriginPlanetDiameter(startEp, endEp) {
-    let people;
-    // people = Array.from(new Set(
-    return films
-        .filter(film => film.episode_id >= startEp && film.episode_id <= endEp)
-        .flatMap(film => film.characters)
-        .filter((url, index, array) => {
-            return array.indexOf(url) === index
-        })
-    .map(characterUrl => {
-        const person = people2.find(person => person.url === characterUrl);
-        if (person) {
-
-        return {
-            name: person.name,
-            homeworld: person.homeworld
-        }
-        }
-    })
-    .map(person => {
-        const planet = planets.find(planet => planet.url === person.homeworld);
-
-        if (planet.diameter > 0 && /^[0-9]+$/.test(planet.diameter)) {
-
-            return {
-                name: person.name,
-                diameter: planet.diameter
-
-        }}
-    })
-
-    // .sort((a, b) => b.diameter - a.diameter)
-    // ));
-    // return Array.from(new Set(result))
-
-    return people;
+    return Array.from(
+        films
+            .filter(film => film.episode_id >= startEp && film.episode_id <= endEp)
+            .flatMap(film => film.characters)
+            .filter((url, index, array) => {
+                return array.indexOf(url) === index
+            })
+            .map(characterUrl => {
+                const person = people.find(person => person.url === characterUrl);
+                if (person) {
+                    return {
+                        name: person.name,
+                        planet: planets.find(planet => planet.url === person.homeworld).name,
+                        diameter: planets.find(planet => planet.url === person.homeworld).diameter
+                    }
+                }
+            })
+            .filter(planet => planet.diameter > 0 && /^[0-9]+$/.test(planet.diameter))
+            .map(person => person.name)
+            .sort((a, b) => a.diameter - b.diameter)
+    );
 }
